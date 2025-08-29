9ff0aee5dc1fc24ff24783bbab6949b961688bb4
Lucasdlmoreira <lucas.github05@gmail.com>
2025-08-28 21:42:09 -0300

    adicionando os codigos da aula04

---
+++ aulas/aula04/tests/calculadora.test.js
@@ -0,0 +1,29 @@
+const calculadora = require('../src/calcularoda.js');
+
+describe("Testa as funções da calculadora.js", function(){
+
+test("2+2 = 4?", function() {
+    expect(calculadora.soma(2,2)).toBe(4);
+});
+
+test("2+0 = 2?", function() {
+    expect(calculadora.soma(2,0)).toBe(2);
+});
+
+test("-2+4 = 2?", function() {
+    expect(calculadora.soma(-2,4)).toBe(2);
+});
+
+test("multiplica inteiro com inteiro da inteiro", function(){
+    expect(calculadora.multiplicacao).toBeDefined();
+    expect(calculadora.multiplicacao(2,2)).toBe(4);
+    expect(calculadora.multiplicacao(2,0)).toBe(0);
+    expect(calculadora.multiplicacao(2,-1)).toBe(-2);
+    expect(calculadora.multiplicacao(-2,-1)).toBe(2);
+});
+
+test("Não pode dividir por zero", function(){
+    expect(calculadora.divisao).toBeDefined();
+    expect(() => calculadora.divisao(2,0)).toThrow("Divisão por zero!");
+});
+});
